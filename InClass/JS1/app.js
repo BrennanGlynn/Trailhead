ad0826242da7fb9d1a56636f0d0b5ab090737029
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-07-16 15:20:57 -0600

    Added skills section

--- InClass/JS1/app.js
+++ InClass/JS1/app.js
@@ -33,46 +33,11 @@ about.append(summaryLocation);
 about.append(summaryPhone);
 about.append(summaryEmail);
 
-/*-- /Summary --*/
+/*-- Skills --*/
+var skillsList = document.getElementById('skills-list');
 
-
-// /*-- Skills --*/
-//
-// // Step 1: Create Elements
-// var publicSpeaking = document.createElement('li');
-// var management = document.createElement('li');
-// var selling = document.createElement('li');
-// var mentorship = document.createElement('li');
-// var personalCoaching = document.createElement('li');
-// var songWriting = document.createElement('li');
-// var jokeTelling = document.createElement('li');
-// var storyTelling = document.createElement('li');
-// var inspiringOthers = document.createElement('li');
-//
-// // Step 2: Integrate Data into Elements
-// publicSpeaking.innerHTML = resumeData.skills[0];
-// management.innerHTML = resumeData.skills[1];
-// selling.innerHTML = resumeData.skills[2];
-// mentorship.innerHTML = resumeData.skills[3];
-// personalCoaching.innerHTML = resumeData.skills[4];
-// songWriting.innerHTML = resumeData.skills[5];
-// jokeTelling.innerHTML = resumeData.skills[6];
-// storyTelling.innerHTML = resumeData.skills[7];
-// inspiringOthers.innerHTML = resumeData.skills[8];
-//
-//
-// // Step 3: Get and/or Create Parent Element(s)
-// var skillsList = document.getElementById('skills-list');
-//
-// // Step 4: Append Child Elements to Parent Element(s)
-// skillsList.append(publicSpeaking);
-// skillsList.append(management);
-// skillsList.append(selling);
-// skillsList.append(mentorship);
-// skillsList.append(personalCoaching);
-// skillsList.append(songWriting);
-// skillsList.append(jokeTelling);
-// skillsList.append(storyTelling);
-// skillsList.append(inspiringOthers);
-//
-// /*-- /Skills --*/
+for (var i in data.skills) {
+  var skill = document.createElement("li");
+  skill.innerHTML = data.skills[i];
+  skillsList.append(skill);
+}
