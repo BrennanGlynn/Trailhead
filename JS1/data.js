b93c8e51bf70f677863e54b566b1c1921ddcb8c6
Brennan Glynn <brennanglynn@u.boisestate.edu>
2017-07-17 14:27:33 -0600

    Changed data to be about me

--- JS1/data.js
+++ JS1/data.js
@@ -1,49 +1,49 @@
 let data = {
-  'summary': {    
-    'background': 'http://i.huffpost.com/gen/2877070/images/n-SCRANTON-PA-628x314.jpg',
-    'profile': 'https://upload.wikimedia.org/wikipedia/en/d/dc/MichaelScott.png',
-    'title': 'Regional Manager',
-    'name': 'Michael Gary Scott',
-    'location': 'Scranton, PA',
-    'phone': '(570) 867-5309',
-    'email': 'mscott@dundermifflin.com'
+  'summary': {
+    'background': 'https://image.ibb.co/nhtpza/91ba221a6a722d81539c27cdff36850b.jpg',
+    'profile': 'https://image.ibb.co/bJMnkF/resumepicture.jpg',
+    'title': 'Web Developer',
+    'name': 'Brennan B. Glynn',
+    'location': 'Boise, ID',
+    'phone': '(208) 850-8677',
+    'email': 'brennanglynn@u.boisestate.edu'
   },
-  'skills': ['Public Speaking', 'Management', 'Selling', 'Mentorship', 'Personal Coaching', 'Song Writing', 'Joke Telling', 'Story Telling', 'Inspiring Others'],
+  'skills': ['JavaScript', 'HTML', 'CSS','Node.js', 'Express.js', 'MongoDB',  'Vue.js', 'React.js'],
   'experience': [
-      {
-          'occupation': 'Regional Manager',
-          'company': 'Dunder Mifflin',
-          'startDate': 2000,
-          'endDate': 2009,
-          'responsibilities': [
-              'Responsible for making the office a place where dreams come true.',
-              'Threw the most amazing office parties to improve office morale.'
-
-          ]
-      },
-      {
-          'occupation': 'Salesman',
-          'company': 'Dunder Mifflin',
-          'startDate': 1992,
-          'endDate': 2000,
-          'responsibilities': [
-              'Won consecutive "Best Salesman of the Year" awards.',
-              'Singlehandedly acquired key clients using unorthodox methods'
-
-          ]
-      }
-
-],
+    {
+      'occupation': 'Mower of Lawns',
+      'company': 'Brennan\'s Lawns',
+      'startDate': 2012,
+      'endDate': 2017,
+      'responsibilities': [
+        'Manicuring lawns',
+        'Discovering dry spots',
+        'Killer lawn patterns'
+      ]
+    }
+  ],
   'education': [
-      {
-        'degree': 'High School Diploma',
-        'school': 'Scranton High School',
-        'startDate': 1978,
-        'endDate': 1982,
-        'accomplishments': [
-            'Scored more goals than anyone in the history of the hockey team.',
-            'Lost college tuition money in a pyramid scheme.'
-        ]
-      }
+    {
+      'degree': 'High School Diploma',
+      'school': 'Mountain View High School',
+      'startDate': 2010,
+      'endDate': 2014,
+      'accomplishments': [
+        'Played on the varsity tennis, hockey, and lacrosse teams',
+        'Two time state champion (lacrosse)',
+      ]
+    },
+    {
+      'degree': 'Finance B.B.A.',
+      'school': 'Boise State University',
+      'startDate': 2014,
+      'endDate': 2018,
+      'accomplishments': [
+        'Dean\'s List with Honors (Spring 2016)',
+        'Dean\'s List with Honors (Fall 2016)',
+        'Dean\'s List with Honors (Spring 2017)',
+        'Earned a 3.8 without purchasing a single textbook (Spring 2017)',
+      ]
+    }
   ]
 };
